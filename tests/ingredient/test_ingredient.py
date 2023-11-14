f2616cf39acbe5551e774d2964cab0153d58e7ca
matheus proença <matheusproenca2020@hotmail.com>
2023-11-14 16:44:48 -0300

    Finalizando Req01

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,5 +1,23 @@
-from src.models.ingredient import Ingredient
+from src.models.ingredient import (
+    Ingredient,
+    Restriction,
+)  # noqa: F401, E261, E501
 
 
+# Req 1
 def test_ingredient():
-    pass
+    ingredientNum1 = Ingredient("salmão")
+    ingredientNum2 = Ingredient("bacon")
+    ingredientNum3 = Ingredient("salmão")
+
+    assert ingredientNum1.restrictions == {
+        Restriction.ANIMAL_MEAT,
+        Restriction.SEAFOOD,
+        Restriction.ANIMAL_DERIVED,
+    }
+    assert ingredientNum1.name == "salmão"
+    assert ingredientNum1 != ingredientNum2
+    assert ingredientNum1 == ingredientNum3
+    assert hash(ingredientNum1) == hash(ingredientNum3)
+    assert hash(ingredientNum2) != hash(ingredientNum1)
+    assert Ingredient.__repr__(ingredientNum1) == "Ingredient('salmão')"
